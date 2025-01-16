50bf1f4eb69958870139b4007ba569814be12a42
shiroHSG <86618821+shiroHSG@users.noreply.github.com>
2025-01-16 18:09:53 +0900

    1152 update

---
+++ src/AlgorithmSolution/P1152.java
@@ -0,0 +1,21 @@
+package AlgorithmSolution;
+
+import java.util.Scanner;
+
+public class P1152 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String str = input.nextLine();
+        //trim() 앞뒤 공백제거, split() 구분자를 기준으로 str을 나눔
+        //\\s+ 공백 줄바꿈 탭 등을 하나의 구분자로 봄
+        String[] words = str.trim().split("\\s+");
+        
+        System.out.println(words.length);
+
+        for(String s : words) {
+            System.out.println(s);
+        }
+        input.close();
+    }
+}
