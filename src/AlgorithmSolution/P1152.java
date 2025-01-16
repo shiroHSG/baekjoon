9de39815c08c23874d504b1303329adcbae00138
shiroHSG <86618821+shiroHSG@users.noreply.github.com>
2025-01-16 18:16:18 +0900

    update

--- src/AlgorithmSolution/P1152.java
+++ src/AlgorithmSolution/P1152.java
@@ -7,14 +7,15 @@ public class P1152 {
         Scanner input = new Scanner(System.in);
 
         String str = input.nextLine();
+
+        if(str.trim().isEmpty()) {
+            System.out.println(0);
+        }
+        else {
         //trim() 앞뒤 공백제거, split() 구분자를 기준으로 str을 나눔
         //\\s+ 공백 줄바꿈 탭 등을 하나의 구분자로 봄
         String[] words = str.trim().split("\\s+");
-        
         System.out.println(words.length);
-
-        for(String s : words) {
-            System.out.println(s);
         }
         input.close();
     }
