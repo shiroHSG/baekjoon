91622b2db61c215600e726d53f8db049a21e2479
shiroHSG <86618821+shiroHSG@users.noreply.github.com>
2025-01-16 18:29:23 +0900

    update 1152

--- src/AlgorithmSolution/P1152.java
+++ src/AlgorithmSolution/P1152.java
@@ -12,10 +12,10 @@ public class P1152 {
             System.out.println(0);
         }
         else {
-        //trim() 앞뒤 공백제거, split() 구분자를 기준으로 str을 나눔
-        //\\s+ 공백 줄바꿈 탭 등을 하나의 구분자로 봄
-        String[] words = str.trim().split("\\s+");
-        System.out.println(words.length);
+            //trim() 앞뒤 공백제거, split() 구분자를 기준으로 str을 나눔
+            //\\s+ 공백 줄바꿈 탭 등을 하나의 구분자로 봄
+            String[] words = str.trim().split("\\s+");
+            System.out.println(words.length);
         }
         input.close();
     }
