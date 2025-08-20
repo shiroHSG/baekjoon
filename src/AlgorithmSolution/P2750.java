30d4dbc21425a950ea669e8f48a141981f1d3b31
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-08-20 12:49:48 +0900

    solve 2750

---
+++ src/AlgorithmSolution/P2750.java
@@ -0,0 +1,22 @@
+package AlgorithmSolution;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P2750 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int num : arr)
+            System.out.println(num);
+    }
+}
