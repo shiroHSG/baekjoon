1814c08077008642401e452a77a60af4352c4706
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-08-20 15:39:52 +0900

    solve 10989

---
+++ src/AlgorithmSolution/P10989.java
@@ -0,0 +1,28 @@
+package AlgorithmSolution;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P10989 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] countArr = new int[10001];
+
+        for(int i=0; i<N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            countArr[num]++;
+        }
+
+        for(int i=0; i<countArr.length; i++) {
+            while(countArr[i]-->0) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
