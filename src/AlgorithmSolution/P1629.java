5ec33c78fad46e5966bc4737154787a2851005bd
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-08-24 09:46:27 +0900

    solve 1629

--- src/AlgorithmSolution/P1629.java
+++ src/AlgorithmSolution/P1629.java
@@ -8,21 +8,33 @@ import java.util.StringTokenizer;
     A를 B번 곱한 수를 C로 나눈 나머지를 출력
  */
 public class P1629 {
-    static int A, B, C;
+    static long C;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        A = Integer.parseInt(st.nextToken()); // 곱해지는 수
-        B = Integer.parseInt(st.nextToken()); // 몇 번 곱할지
+        long A = Integer.parseInt(st.nextToken()); // 곱해지는 수
+        long B = Integer.parseInt(st.nextToken()); // 몇 번 곱할지
         C = Integer.parseInt(st.nextToken()); // 나눌 수
 
-        int r = modNum(B);
+        long r = modNum(A, B);
         System.out.println(r);
     }
 
-    public static int modNum(int mod) {
-
+    /*
+        a : 곱하는 수
+        b : 곱해지는 횟수
+     */
+    public static long modNum(long a, long b) {
+        a = a%C;
+        long res = 1 % C;
+        while(b>0) {
+            if((b & 1) == 1)
+                res = (res * a) % C;
+            a = a*a % C;
+            b = b >> 1;
+        }
+        return res;
     }
 }
