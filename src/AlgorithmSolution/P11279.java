24933d9ef880d595ba3a3a69e6a7144c902c62a1
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-09-04 13:10:11 +0900

    solve 11279

---
+++ src/AlgorithmSolution/P11279.java
@@ -0,0 +1,29 @@
+package AlgorithmSolution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class P11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b- a);
+
+        for(int i=0; i<N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0) {
+                if(!pq.isEmpty()) sb.append(pq.poll()).append("\n");
+                else sb.append(0).append("\n");
+            }
+            else {
+                pq.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
